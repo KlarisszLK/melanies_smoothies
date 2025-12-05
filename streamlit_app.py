e021dfe05ac8f375fa04e9284792ae10d87e95f0
KlarisszLK <csirvol@yahoo.com>
2025-12-05 14:13:07 +0000

    Both columns in dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ st.write('The name on your Smoothie will be:',name_on_order )
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 # Convert the Snowpark Dataframe to Pandas Dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(data=pd_df, use_container_width=True)
-st.stop()
+# st.dataframe(data=pd_df, use_container_width=True)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Coose up to 5 ingedients:'
